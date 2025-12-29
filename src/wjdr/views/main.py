19b4eb9a7e16c72a1fd89341adb2ab06196df0b3
PHILIPPE Jean-Baptiste <jb.myst89@gmail.com>
2025-12-29 12:53:57 +0000

    feat: implement campaign creation functionality and update navigation

--- src/wjdr/views/main.py
+++ src/wjdr/views/main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from nicegui import app, ui
 
+from wjdr.views.pages.campain_creation import campain_page
 from wjdr.views.pages.character_creation import character_page
 from wjdr.views.theme import frame
 
@@ -15,18 +16,16 @@ def character_creation() -> None:
     character_page()
 
 
+@ui.page("/campain-creation")
+def campain_creation() -> None:
+    """Campaign creation page."""
+    campain_page()
+
+
 def index() -> None:
     with ui.row().classes("items-center justify-center gap-4"):
-        ui.link("Créer une fiche de personnage", "/character").classes("text-lg font-bold text-primary mt-6")
-    # ui.markdown('# Welcome to WJDR').classes('text-primary font-bold')
-    # ui.markdown('This is the main page of the WJDR application.').classes('text-info')
-    # ui.markdown('This place is very dark, be careful!)').classes('text-warning')
-    # ui.markdown('If you need help, contact support.').classes('text-positive')
-    # ui.markdown('If something goes wrong, please report a bug.').classes('text-negative')
-    # ui.markdown('Enjoy your stay!').classes('text-accent')
-    # ui.markdown('The theme colors are inspired by classic RPG aesthetics.').classes('text-secondary')
-    # ui.markdown('Explore the features and have fun!').classes('text-primary')
-    # ui.markdown('This sentence is in accent color.').classes('text-accent')
+        ui.link("Créer une fiche de personnage", "/character-creation").classes("text-lg font-bold text-primary mt-6")
+        ui.link("Créer une campagne", "/campain-creation").classes("text-lg font-bold text-primary mt-6")
 
 
 @ui.page("/")
--- src/wjdr/views/pages/__init__.py
+++ src/wjdr/views/pages/__init__.py
@@ -2,4 +2,6 @@
 
 * Character page : This page displays the character sheet."""
 
+from .campain_creation import campain_page  # noqa: F401
 from .character_creation import character_page  # noqa: F401
+from .helpers import store_state  # noqa: F401
