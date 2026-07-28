686ce7b8e0da014dcd82b9fb8edbe3615b771cb6
PHILIPPE Jean-Baptiste <jb.myst89@gmail.com>
2026-07-28 11:21:42 +0000

    feat(db): add career data + career compendium
    * add migration to create table
    * add python script to generate+insert data into career_characteristics and career_paths

---
+++ migrations/006_add_career_characteristics.sql
@@ -0,0 +1,14 @@
+-- Migration 006 :  Add career characteristics to the database
+-- This migration adds a new table to store career characteristics and updates the existing careers table to include a foreign key reference to the new characteristics table.
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS public.career_characteristics (
+    career_id UUID NOT NULL REFERENCES public.careers(id) ON DELETE CASCADE,
+    stat_code INT REFERENCES public.stats(code) ON DELETE CASCADE,
+    value INT NOT NULL,
+    CONSTRAINT career_characteristics_pkey PRIMARY KEY (career_id, stat_code),
+    CONSTRAINT career_characteristics_value_check CHECK (value >= 0 AND value <= 100)
+) TABLESPACE pg_default;
+
+COMMIT;
